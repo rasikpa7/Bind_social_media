0e20d4407ace55945334737a350046f98d6bae04
rasikpa7 <rasikpa7@gmail.com>
2022-07-05 12:26:01 +0530

    loginscreen

--- lib/view/loginScreen/logInScreen.dart
+++ lib/view/loginScreen/logInScreen.dart
@@ -8,10 +8,47 @@ class LogInScree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        
-      ],),
-    );
+      body:
+        SafeArea(
+          child: ListView(
+          children: [
+              Container(height: 250,color: Colors.grey.withOpacity(0.4),),
+              Container(alignment: Alignment.topLeft,
+
+              
+
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text('Login',style: TextStyle(fontWeight: FontWeight.w900,fontSize: 30,color: Color.fromARGB(255, 11, 90, 155)),),
+                    SizedBox(height: 20,),
+                 TextFormField(
+                  decoration: InputDecoration(
+                    label: Text('Email'),
+                    prefixIcon: Icon(Icons.email),
+                    hintText: 'example.gmail.com',
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))
+                  ),
+                 ),
+                    SizedBox(height: 15,),
+                     TextFormField(obscureText: true,
+                  decoration: InputDecoration(
+                    label: Text('Password'),
+                    prefixIcon: Icon(Icons.password),
+                    
+                    hintText: 'abc1239ab',
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))
+                  ),
+                 ),
+                    ],
+                  ),
+                ),
+              )
+          ],),
+        ),
+      );
+ 
   }
 }

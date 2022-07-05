f36b64f4ffc6c596fbbc6c9e1734d0dae4cb6afb
rasikpa7 <rasikpa7@gmail.com>
2022-07-05 13:19:51 +0530

    loginscreencompleted

--- lib/view/loginScreen/logInScreen.dart
+++ lib/view/loginScreen/logInScreen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:lottie/lottie.dart';
 class LogInScree extends StatelessWidget {
   const LogInScree({Key? key}) : super(key: key);
 
@@ -12,16 +13,24 @@ class LogInScree extends StatelessWidget {
         SafeArea(
           child: ListView(
           children: [
-              Container(height: 250,color: Colors.grey.withOpacity(0.4),),
-              Container(alignment: Alignment.topLeft,
+              Container(decoration: BoxDecoration(borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25)
+              ),
+              color: Colors.grey.withOpacity(0.3),),
+                height: 250,
+              child: Lottie.network('https://assets5.lottiefiles.com/packages/lf20_dn6rwtwl.json',fit: BoxFit.contain),),
+              Container(
+                alignment: Alignment.topLeft,
 
-              
+
 
                 child: Padding(
                   padding: const EdgeInsets.all(18.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
+                    SizedBox(height: 10,),
                       Text('Login',style: TextStyle(fontWeight: FontWeight.w900,fontSize: 30,color: Color.fromARGB(255, 11, 90, 155)),),
                     SizedBox(height: 20,),
                  TextFormField(
@@ -38,10 +47,52 @@ class LogInScree extends StatelessWidget {
                     label: Text('Password'),
                     prefixIcon: Icon(Icons.password),
                     
-                    hintText: 'abc1239ab',
+                    hintText: 'password',
                     border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))
                   ),
                  ),
+                  SizedBox(height: 15,),
+                 Container(width: 320,
+                 decoration: BoxDecoration(
+                
+                  borderRadius: BorderRadius.circular(30)
+                 ),
+                 height: 45,
+                   child: ClipRRect(borderRadius: BorderRadius.circular(15),
+                     child: ElevatedButton(style:ElevatedButton.styleFrom(
+                      primary: Colors.teal,
+                     ) ,
+                      onPressed: (){
+                   
+                     }, child: Text('LogIn',style: TextStyle(fontWeight: FontWeight.bold),)),
+                   ),
+                 ),
+   SizedBox(height: 15,),
+                 Text('Or sign in with'),
+                 SizedBox(height: 10,),
+                 Row(mainAxisAlignment: MainAxisAlignment.center,
+                   children: [
+                Container(
+                  width: 100,
+                decoration: BoxDecoration(color: Colors.green,
+                  borderRadius: BorderRadius.circular(15)),
+                  child: IconButton(onPressed: (){
+
+                  }, icon: FaIcon(FontAwesomeIcons.google,color: Colors.white,)),
+                ),
+                SizedBox(width: 5,),
+                     Container(
+                      width: 100,
+                decoration: BoxDecoration(color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(15)),
+                  child: IconButton(onPressed: (){
+
+                  }, icon: FaIcon(FontAwesomeIcons.facebook,color: Colors.white,)),
+                )
+                   ],
+                 )
+
+              
                     ],
                   ),
                 ),

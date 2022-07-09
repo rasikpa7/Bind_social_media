91210b6fe73a898343cdfa1345ab1ac310dcf3d2
rasikpa7 <rasikpa7@gmail.com>
2022-07-09 12:00:35 +0530

    ntg

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -11,15 +11,28 @@ class AuthMethods {
 
   //Sign up user 
  Future<String> signUpUser(
-      {required String email,
-      required String password,
-      required String username,
-      required String bio,
-      required Uint8List file})async {
+      {
+       required String email,
+       required String password,
+       required String username,
+       required String bio,
+       required Uint8List file
+      })async {
 String res='Some error occured';
 try{
   if(email.isEmpty||password.isEmpty||username.isEmpty||bio.isEmpty||file!=null){
   UserCredential cred= await _auth.createUserWithEmailAndPassword(email: email, password: password);
+ await _firestore.collection('users').doc(cred.user!.uid).set({
+'username':username,
+'uid':cred.user!.uid,
+'email':email,
+'bio':bio,
+'followers':[],
+'following':[],
+  });
+
+  res='success';
+
   }
 
 }catch(err){
